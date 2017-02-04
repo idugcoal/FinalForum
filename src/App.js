3acba2ccd00ddb214bb76656358bdd8fb4b824be
Doug Cole <idugcoal@gmail.com>
2017-02-04 03:22:58 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,15 @@ import ThreadDisplay from './components/ThreadDisplay';
 import firebase from 'firebase/app';
 import 'firebase/database';
 import './App.css';
+import config from './config.js'
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    const config = 
-    {
-      apiKey: "AIzaSyAyQ0ORIQHxAHggK0k2-cPsKU_U3hn3Bg0",
-      authDomain: "finalforum-c5829.firebaseapp.com",
-      databaseURL: "https://finalforum-c5829.firebaseio.com",
-      storageBucket: "finalforum-c5829.appspot.com",
-      messagingSenderId: "260270446235"
-    };
-    
-    this.app = firebase.initializeApp(config);
+    var keys = config
+
+    this.app = firebase.initializeApp(keys);
     this.database = this.app.database();
 
   }
